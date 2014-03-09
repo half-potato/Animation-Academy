86d3e9d4b68d093139ce8aff8926ff0325da9030
artiespeedy <alexandertmai@gmail.com>
2014-03-08 16:16:32 -0800

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -23,14 +23,62 @@ function AnimationStep(target, x, y, rotation, hidden) {
 }
 
 //TODO: Will take the animationArray and apply it the canvas
+var animationArray = new Array();
+var layerArray = new Array();
+
+//Shape class, contains information to be built into a layer, if the type is image, then the style is the source
+function Shape(type, style, objectName, width, height, x, y)
+{
+	this.type = type;
+	this.style = style;
+	this.objectName = objectName;
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+}
+
+//AnimationStep class; contains instructions for each step. The last instruction's speed is the one that determines when the next step is executed
+function AnimationStep(target, x, y, rotation, color, hidden, speed)
+{
+	this.target = target;
+	this.x = x;
+	this.y = y;
+	this.rotation = rotation;
+	this.color = color;
+	this.hidden = hidden;
+	this.speed = speed;
+}
+
+//TODO: Will take the animationArray and apply it the canvas
 function animate(array) {
-	var step = animationArray.shift(), i;
-	for (i = 0; i < step.length; i++) {
-		if(i == step.length - 1) {
+	console.log("Animating");
+	var arrayOfSteps = array.shift(), i;
+	for (i in arrayOfSteps)
+	{
+		var step = arrayOfSteps[i];
+		$("canvas").animateLayer(step.target, {
+			x: step.x, y: step.y,
+			fillStyle: step.color,
+			rotate: step.rotate
+		}, step.speed, animate(array));
+	}
+}
 
-		} else {
-
-		}
+//Takes the array of Shape objects and turns them into layers, which are then added to the canvas and drawn.
+function addToCanvas(array)
+{
+	var object;
+	for (object in array) {
+		$('canvas').addLayer({
+			type: array[object].type,
+			fillStyle: array[object].style,
+			x: array[object].x,
+			y: array[object].y,
+			width: array[object].width,
+			height: array[object].height,
+			draggable: true
+		}).drawLayers();
 	}
 }
 
@@ -62,6 +110,7 @@ function addShape(shape) {
 		}).drawLayers();
 	} else {
 		layerArray[layerArray.length] = shape;
+		console.log("Adding shape");
 		$('canvas').addLayer({
 			type: shape.type,
 			fillStyle: shape.style,
@@ -78,20 +127,19 @@ function redraw(array) {
 }
 
 //Temporary objects added
-animationArray[0] = new AnimationStep('rectangle', 30, 30, 0, false);
+animationArray[0] = new AnimationStep('box', 30, 30, 0, "#000000", false, 100);
 
 function addRect(){
 	var color = document.getElementById("color");
 	var X = document.getElementById("X");
 	var Y = document.getElementById("Y");
 	var outputColor=color.value;
-	var outputX=X.value;
-	var outputY=Y.value;
-	addShape(new Shape("rectangle", outputColor, "box", 20, 20, outputX, outputY));
-	
+	var outputX = parseInt(X.value);
+	var outputY = parseInt(Y.value);
+	addShape(new Shape("ellipse", outputColor, "box", 20, 20, outputX, outputY));
 }
 
 $(document).ready(function() {
 	addToCanvas(layerArray);
-	redraw(layerArray);
+	$("#hi").on(animate(animationArray));
 });
